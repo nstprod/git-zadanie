988e8f85b973ef24967e01602084997d33d863f9
nstprod <milana.prod@gmail.com>
2024-12-15 15:17:21 +0100

    Implementacja funkcji 'eliminate' w pliku gauss.c
    Funkcja wykonuje eliminację Gaussa bez selekcji elementu diagonalnego

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,20 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+    int i, j, k;
+    for (i = 0; i < mat->c-1; i++){
+    	double pivot = mat->data[i][i];
+    	if (pivot == 0)
+    		return 1;
+    	for (j = i+1; j < mat->r; j++){
+    		double zero_el = mat->data[j][i];
+    		double skalar = zero_el / pivot;
+    		b->data[j][0]=b->data[j][0] - skalar * b->data[i][0];
+    		for (k=i; k < mat->c; k++){
+	    		mat->data[j][k]=mat->data[j][k] - skalar * mat->data[i][k];
+	    	}
+    	}
+    }
+	return 0;
 }
 
