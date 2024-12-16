a7b2c31e78ce809eecc2392aceaeb0d1c2e28fe9
Konrad <01197862@pw.edu.pl>
2024-12-16 22:12:39 +0000

    Implementacja funkcji 'backsubst' w pliku backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,18 +5,25 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	int i, j;
+	int row = mat->r;
+	int collumn = mat->c;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	if(row != collumn) {
+		return 2;
+	}
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	for(i = row - 1; i >= 0; i--) {
+		double value = b->data[i][0];
+		for(j = i + 1; j <= collumn - 1; j++) {
+			value -= mat->data[i][j] * x->data[j][0];
+		}
+		if (mat->data[i][i] == 0) {
+			return 1;
+		}
+		x->data[i][0] = value / mat->data[i][i];
+	}
+	return 0;
 }
 
 
